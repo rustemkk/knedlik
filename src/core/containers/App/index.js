6e046ce329f3debc662d2b20bb2a0341575e7b96
Rustem Islamuratov <rustem.islamuratov@gmail.com>
2018-12-23 23:05:02 +0700

    Configure sass

--- src/core/containers/App/index.js
+++ src/core/containers/App/index.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 
-// import '../../../styles.css';
-// import logo from './logo.svg';
+import logo from 'images/logo.svg';
+
+import s from './index.scss';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        awdawd123123
-        {/*<header className="App-header">*/}
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          {/*<p>*/}
-            {/*Edit <code>src/App.js</code> and save to reload.*/}
-          {/*</p>*/}
-          {/*<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">*/}
-            {/*Learn React*/}
-          {/*</a>*/}
-        {/*</header>*/}
+      <div className={s.App}>
+        <header className={s.AppHeader}>
+          <img src={logo} className={s.AppLogo} alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className={s.AppLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
