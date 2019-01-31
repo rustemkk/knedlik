eacc8e2655682127da820e78daeb5d0b9f992991
Rustem Islamuratov <rustem.islamuratov@gmail.com>
2019-01-31 22:39:15 +0700

    Add SvgIcon component

---
+++ src/ui/components/SvgIcon/index.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+
+const icons = [
+  'close',
+  'card',
+  'dollar',
+  'plus',
+  'wallet',
+].reduce((acc, curr) => ({ ...acc, [curr]: require(`./svg/${curr}.svg`) }), {});
+
+const SvgIcon = ({ className, name, size }) => (
+  <span
+    className={className}
+    style={{
+      maskImage: `url(${icons[name]})`,
+      maskSize: size ? `${size}px` : '25px',
+      maskRepeat: 'no-repeat',
+      WebkitMaskImage: `url(${icons[name]})`,
+      WebkitMaskSize: size ? `${size}px` : '25px',
+      WebkitMaskRepeat: 'no-repeat',
+      width: size ? `${size}px` : '25px',
+      height: size ? `${size}px` : '25px',
+      backgroundColor: '#FFF',
+      display: 'block'
+    }}
+  />
+);
+
+SvgIcon.propTypes = {
+  className: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  size: PropTypes.number,
+};
+
+export default SvgIcon;
